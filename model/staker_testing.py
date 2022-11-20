89a10b17659dc2929ea5131e26cdf4e13754f7a9
Mugundan Kottur Suresh <mugundan.kottur@gmail.com>
2022-11-21 01:01:07 +0530

    Testing code on external database code ready

--- model/staker_testing.py
+++ model/staker_testing.py
@@ -17,19 +17,15 @@ opt = {
     'image_size': 224,
     'is_grayscale': False,
     'image_parsed': False,
-    'dataset_size': 50000,
-    'train': True,
-    'epochs': 30,
-    'load': False
+    'dataset_size': 5000
 }
 
 # PATHS
 
 ABSOLUTE_PATH = '/home/reshyurem/Accurate-smiles-recognition-Img2Mol/'
-IMAGE_PATH = ABSOLUTE_PATH + 'data/train_images/'
-IMAGE_TENSOR_FILE = IMAGE_PATH + 'image_tensor.pt'
-CDDD_PATH = ABSOLUTE_PATH + 'data/embeddings.csv'
 MODEL_FILE = ABSOLUTE_PATH + 'data/50000_images_30_epochs.pt'
+STAKER_IMAGE_PATH = ABSOLUTE_PATH + 'data/Staker'
+STAKER_CDDD_PATH = ABSOLUTE_PATH + 'data/Staker_embeddings.csv'
 
 # Helper Functions
 
@@ -56,27 +52,6 @@ def load_image(path):
 
 # Initialize a Cuda device if available
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
-
-# Load CDDD embeddings for training and testing
-
-cddd_df = pd.read_csv(CDDD_PATH, delimiter=',',  header=None)
-cddd = torch.from_numpy(cddd_df.values).float().to(device)
-cddd = cddd[:opt['dataset_size']]
-
-# Load corresponding images for training and testing
-# Load the images from a tensor file if it exists
-
-images = []
-
-if opt['image_parsed']:
-    images = torch.load(IMAGE_TENSOR_FILE)
-else:
-    indices = range(cddd_df.shape[0])
-    image_list = []
-    for i in indices:
-        image_list.append(os.path.join(IMAGE_PATH,"{}.png".format(i)))
-    images = torch.cat([torch.unsqueeze(load_image(image), 0) for image in image_list[:opt['dataset_size']]], dim=0)
-    torch.save(images, IMAGE_TENSOR_FILE)
 
 # Define the model
 
@@ -133,64 +108,31 @@ learning_rate = 1e-4
 criterion = nn.MSELoss()
 optimizer = torch.optim.AdamW(params=model.parameters(), lr=learning_rate)
 
-# Load the model if needed
+# Load the model
 
-if opt["load"] == True:
-    model.load_state_dict(torch.load(MODEL_FILE))
+model.load_state_dict(torch.load(MODEL_FILE))
 
-# Split the dataset into training and testing and validation
+# Load the images and cddd values
 
-dataset = []
-for i in range(len(images)):
-    dataset.append([images[i], cddd[i]])
-train_size = int(0.8 * len(images))
-test_size = len(images) - train_size
-train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
-train_dataset, val_dataset = torch.utils.data.random_split(train_dataset, [int(train_size*0.9), int(train_size*0.1)])
+indices = range(5000)
+image_list = []
+for i in indices:
+    image_list.append(os.path.join(STAKER_IMAGE_PATH,"{}.png".format(i)))
+staker_images = torch.cat([torch.unsqueeze(load_image(image), 0) for image in image_list], dim=0)
 
-# Train the model
+staker_cddd_df = pd.read_csv(STAKER_CDDD_PATH, delimiter=',',  header=None)
+staker_cddd = torch.from_numpy(staker_cddd_df.values).float().to(device)
+staker_cddd = staker_cddd[:opt['dataset_size']]
 
-gc.collect()
-trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=128,shuffle=True)
+b_dataset = []
+for i in range(len(staker_images)):
+    b_dataset.append([staker_images[i], staker_cddd[i]])
 
-if opt["train"] == True:
-    for epoch in tqdm(range(opt['epochs'])):
-        running_loss = 0.0
-        st = time.time()
-        for i, (batch_x, batch_y) in enumerate(trainloader, 0):
-            batch_x = batch_x.to(device)
-            batch_y = batch_y.to(device)
+benchmarkloader = torch.utils.data.DataLoader(b_dataset, batch_size=128,shuffle=True)
 
-            outputs = model(batch_x)
-            loss = criterion(outputs, batch_y)
+# Test the model on the values
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            running_loss += loss.data
-            if i % 50 == 49:
-                print('[%d, %5d] loss: %.3f' %(epoch + 1, i + 1, running_loss / 50))
-                running_loss = 0.0
-            print("Done with Batch {}, epoch {}".format(i, epoch))
-        et = time.time()
-        torch.save(model.state_dict(), MODEL_FILE)
-        print(et-st, "Epoch", epoch)
-
-    print('Finished Training')
-    print('Saving Model...')
-    torch.save(model.state_dict(), MODEL_FILE)
-
-# Test the model
-
-testloader = torch.utils.data.DataLoader(test_dataset, batch_size=128,shuffle=True)
-test_iter = iter(testloader)
-test_images, test_labels = next(test_iter)
-test_images = test_images.to(device)
-
-# Calculate MSE loss
-
-for i, (batch_x, test_labels) in enumerate(testloader, 0):
+for i, (batch_x, test_labels) in enumerate(benchmarkloader, 0):
     batch_x = batch_x.to(device)
     test_labels = test_labels.to(device)
     predictions = model(batch_x)
@@ -198,9 +140,10 @@ for i, (batch_x, test_labels) in enumerate(testloader, 0):
     print(i, loss)
     pred_np = predictions.cpu().detach().numpy()
     test_np = test_labels.cpu().detach().numpy()
-    file1 = open(ABSOLUTE_PATH + "data/test_results/predictions_"+str(i)+".pkl", "wb")
+    file1 = open("staker_benchmark_predictions.pkl", "wb")
     np.save(file1, pred_np, allow_pickle=False)
     file1.close()
-    file2 = open(ABSOLUTE_PATH + "data/test_results/test_labels_"+str(i)+".pkl", "wb")
+    file2 = open("staker_benchmark_test_labels.pkl", "wb")
     np.save(file2, test_np, allow_pickle=False)
     file2.close()
+    break
